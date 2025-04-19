2f176ff6f97aedf7ac29e7f51d1a6d6fde417d57
grahamSpecter <graham-healiss@live.co.uk>
2025-04-19 22:04:23 +0100

    Changed based on feedback from Hazelify

---
+++ Core/ConfigHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using BepInEx.Logging;
+
+namespace SPTStances
+{
+    public static class ConfigHandler
+    {
+        public static void CheckConfig(string configPath, ManualLogSource log)
+        {
+            if (!File.Exists(configPath))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(configPath));
+
+                var defaultConfig = new
+                {
+                    enableStances = true,
+                    defaultStance = "Default",
+                    multipliers = new
+                    {
+                        HighReady = new { sway = 0.8, ergo = 1.1 },
+                        LowReady = new { sway = 0.6, ergo = 0.9 },
+                        Compressed = new { sway = 0.5, ergo = 0.75 },
+                        Default = new { sway = 1.0, ergo = 1.0 }
+                    }
+                };
+
+                File.WriteAllText(configPath, JsonConvert.SerializeObject(defaultConfig, Formatting.Indented));
+                log.LogInfo($"[SPTStances] Created default config at: {configPath}");
+            }
+        }
+    }
+}
